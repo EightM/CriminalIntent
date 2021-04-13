dc2efd4a8195de5754c47dcadd36c6c818e122d2
Viktor <zchokobo@gmail.com>
2021-04-13 13:37:05 +0300

    initial commit

---
+++ app/src/main/java/com/github/eightm/criminalintent/CrimeListViewModel.kt
@@ -0,0 +1,12 @@
+package com.github.eightm.criminalintent
+
+import androidx.lifecycle.ViewModel
+
+class CrimeListViewModel : ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
+    val crimesListLiveData = crimeRepository.getCrimes()
+
+    fun addCrime(crime: Crime) {
+        crimeRepository.addCrime(crime)
+    }
+}
